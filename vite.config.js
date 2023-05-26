4dbe67a28f3dd5e33025fad4682384bb6ef4813e
rodrfct <rodrigoca.fct@gmail.com>
2023-05-26 19:13:11 +0200

    Added `dist` and `static.yml` for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/intro-section-with-dropdown-navigation',
   plugins: [vue()],
   resolve: {
     alias: {
